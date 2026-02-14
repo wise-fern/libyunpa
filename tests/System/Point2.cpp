2d1a56bf8eb81d93d9d5762e9817c54510a3b8c8
wise-fern <wise.fern2345@fastmail.com>
2026-02-14 14:02:37 -0500

    Update Point2.cpp

--- tests/System/Point2.cpp
+++ tests/System/Point2.cpp
@@ -3,70 +3,73 @@
 
 import libyunpa;
 
-using namespace libyunpa;
-
-class Point2Tests
-    : public testing::TestWithParam<std::tuple<int, int, int, int>> {
+class Point2 : public testing::TestWithParam<std::tuple<int, int, int, int>> {
 protected:
-  Point2<int> lhs{};
-  Point2<int> rhs{};
+  libyunpa::Point2<int> lhs{};
+  libyunpa::Point2<int> rhs{};
 
 public:
-  Point2Tests() {
+  Point2() {
     auto [lhsx, lhsy, rhsx, rhsy] = GetParam();
 
-    lhs = Point2{.x = lhsx, .y = lhsy};
-    rhs = Point2{.x = rhsx, .y = rhsy};
+    lhs = libyunpa::Point2{.x = lhsx, .y = lhsy};
+    rhs = libyunpa::Point2{.x = rhsx, .y = rhsy};
   }
 };
 
-TEST_P(Point2Tests, Equality) {
-  EXPECT_EQ(lhs, lhs);
-  EXPECT_EQ(rhs, rhs);
-  EXPECT_NE(lhs, rhs);
+TEST_P(Point2, Equality) {
+  EXPECT_TRUE(lhs == lhs);
+  EXPECT_TRUE(rhs == rhs);
+  EXPECT_FALSE(lhs == rhs);
 }
 
-TEST_P(Point2Tests, Addition) {
-  auto [lhsx, lhsy, rhsx, rhsy] = GetParam();
-
-  auto result = Point2{.x = lhsx + rhsx, .y = lhsy + rhsy};
-  EXPECT_EQ(lhs + rhs, result);
+TEST_P(Point2, Inequality) {
+  EXPECT_TRUE(lhs != rhs);
+  EXPECT_FALSE(lhs != lhs);
+  EXPECT_FALSE(rhs != rhs);
 }
 
-TEST_P(Point2Tests, Negation) {
+TEST_P(Point2, Addition) {
   auto [lhsx, lhsy, rhsx, rhsy] = GetParam();
 
-  auto result = Point2{.x = -lhsx, .y = -lhsy};
-  EXPECT_EQ(-lhs, result);
-  result = Point2{.x = -rhsx, .y = -rhsy};
-  EXPECT_EQ(-rhs, result);
+  auto result = libyunpa::Point2{.x = lhsx + rhsx, .y = lhsy + rhsy};
+  EXPECT_TRUE(lhs + rhs == result);
 }
 
-TEST_P(Point2Tests, Subtraction) {
+TEST_P(Point2, Negation) {
   auto [lhsx, lhsy, rhsx, rhsy] = GetParam();
 
-  auto result = Point2{.x = lhsx - rhsx, .y = lhsy - rhsy};
+  auto result = libyunpa::Point2{.x = -lhsx, .y = -lhsy};
+  EXPECT_TRUE(-lhs == result);
+  result = libyunpa::Point2{.x = -rhsx, .y = -rhsy};
+  EXPECT_TRUE(-rhs == result);
+}
+
+TEST_P(Point2, Subtraction) {
+  auto [lhsx, lhsy, rhsx, rhsy] = GetParam();
+
+  auto result = libyunpa::Point2{.x = lhsx - rhsx, .y = lhsy - rhsy};
   EXPECT_EQ(lhs - rhs, result);
 }
 
-TEST_P(Point2Tests, Multiplication) {
+TEST_P(Point2, Multiplication) {
   auto [lhsx, lhsy, rhsx, rhsy] = GetParam();
 
-  auto result = Point2{.x = lhsx * rhsx, .y = lhsy * rhsy};
+  auto result = libyunpa::Point2{.x = lhsx * rhsx, .y = lhsy * rhsy};
   EXPECT_EQ(lhs * rhs, result);
 }
 
-TEST_P(Point2Tests, ScalarMultiplaction) {
+TEST_P(Point2, ScalarMultiplaction) {
   auto [lhsx, lhsy, rhsx, rhsy] = GetParam();
 
-  auto result = Point2{.x = lhsx * rhsx, .y = lhsy * rhsx};
+  auto result = libyunpa::Point2{.x = lhsx * rhsx, .y = lhsy * rhsx};
   EXPECT_EQ(lhs * rhs.x, result);
 }
 
-TEST_P(Point2Tests, Division) {
+TEST_P(Point2, Division) {
   auto [lhsx, lhsy, rhsx, rhsy] = GetParam();
 
-  auto result = Point2{.x = lhsx / rhsx, .y = lhsy / rhsy};
+  auto result = libyunpa::Point2{.x = lhsx / rhsx, .y = lhsy / rhsy};
   EXPECT_EQ(lhs / rhs, result);
 }
 
@@ -75,7 +78,7 @@ auto rng = std::mt19937(0);
 }
 
 INSTANTIATE_TEST_SUITE_P(System,
-                         Point2Tests,
+                         Point2,
                          testing::Values(std::tuple{rng(), rng(), rng(), rng()},
                                          std::tuple{rng(), rng(), rng(), rng()},
                                          std::tuple{
