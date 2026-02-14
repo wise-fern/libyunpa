3e51f97122514c8fe1935d0e0f5d4ed1fa323a38
wise-fern <wise.fern2345@fastmail.com>
2026-02-14 09:40:23 -0500

    Update Text.cpp

--- tests/Graphics/Text.cpp
+++ tests/Graphics/Text.cpp
@@ -14,8 +14,48 @@ TEST_P(TextTests, GetText) {
 }
 
 TEST_P(TextTests, ToString) {
-  auto result = std::format("\033[1;1H{}\033[0m", GetParam());
+  auto result = std::format("{}\e[0m", GetParam());
   EXPECT_EQ(_text.to_string(), result);
+}
+
+TEST_P(TextTests, Bold) {
+  EXPECT_EQ(false, _text.getBold());
+  _text.setBold();
+  EXPECT_EQ(true, _text.getBold());
+  auto result = std::format("\x1b[1m{}\x1b[0m", GetParam());
+  EXPECT_EQ(result, _text.to_string());
+}
+
+TEST_P(TextTests, Italic) {
+  EXPECT_EQ(false, _text.getItalic());
+  _text.setItalic();
+  EXPECT_EQ(true, _text.getItalic());
+  auto result = std::format("\x1b[2m{}\x1b[0m", GetParam());
+  EXPECT_EQ(result, _text.to_string());
+}
+
+TEST_P(TextTests, Underline) {
+  EXPECT_EQ(false, _text.getUnderline());
+  _text.setUnderline();
+  EXPECT_EQ(true, _text.getUnderline());
+  auto result = std::format("\x1b[3m{}\x1b[0m", GetParam());
+  EXPECT_EQ(result, _text.to_string());
+}
+
+TEST_P(TextTests, DoubleUnderline) {
+  EXPECT_EQ(false, _text.getDoubleUnderline());
+  _text.setDoubleUnderline();
+  EXPECT_EQ(true, _text.getDoubleUnderline());
+  auto result = std::format("\x1b[21m{}\x1b[0m", GetParam());
+  EXPECT_EQ(result, _text.to_string());
+}
+
+TEST_P(TextTests, Blink) {
+  EXPECT_EQ(false, _text.getBlink());
+  _text.setBlink();
+  EXPECT_EQ(true, _text.getBlink());
+  auto result = std::format("\x1b[5{}\x1b[0m", GetParam());
+  EXPECT_EQ(result, _text.to_string());
 }
 
 INSTANTIATE_TEST_SUITE_P(Graphics,
